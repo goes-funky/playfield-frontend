8f75391947f5ec34a0c17597eb9ee491607ee771
Alexander Ciesielski <alexander@ciesielski.co>
2021-06-09 17:10:06 +0300

    technical interview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,26 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
-
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {RecipesComponent} from './recipes/recipes.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
-import {DropdownDirective} from './shared/dropdown.directive';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
-import {AppRoutingModule} from './app-routing.module';
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {RecipeService} from './recipes/recipe.service';
-import { OnChangesComponent } from './learnings/on-changes/on-changes.component';
-import { TeacherStudentTypescriptComponent } from './learnings/teacher-student-typescript/teacher-student-typescript.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { AdvancedTypescriptComponent } from './learnings/advanced-typescript/advanced-typescript.component';
-import {UrlTestComponent} from './learnings/advanced-typescript/models/url-builder';
-import {TeacherStudentDisplayComponent} from './learnings/teacher-student-typescript/components/teacher-student-display/teacher-student-display.component';
+import { UrlTestComponent } from './learnings/advanced-typescript/models/url-builder';
+import { OnChangesComponent } from './learnings/on-changes/on-changes.component';
+import { TeacherStudentDisplayComponent } from './learnings/teacher-student-typescript/components/teacher-student-display/teacher-student-display.component';
+import { TeacherStudentTypescriptComponent } from './learnings/teacher-student-typescript/teacher-student-typescript.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeService } from './recipes/recipe.service';
+import { RecipesComponent } from './recipes/recipes.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -40,16 +39,10 @@ import {TeacherStudentDisplayComponent} from './learnings/teacher-student-typesc
     TeacherStudentTypescriptComponent,
     TeacherStudentDisplayComponent,
     AdvancedTypescriptComponent,
-    UrlTestComponent
+    UrlTestComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule, AppRoutingModule],
   providers: [ShoppingListService, RecipeService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/learnings/advanced-typescript/models/url-builder.ts
+++ src/app/learnings/advanced-typescript/models/url-builder.ts
@@ -1,128 +1,194 @@
-import {Component, Injectable, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 // https://y42.com/dashboard/api/dashboards
 // https://y42.com/dashboard/api/dashboards/{id: string}
 
+export type ID = string | number;
+
+export interface ObjectWithID {
+  id: ID;
+}
+
+export type UrlParams = Record<string, string | number>;
+export type GetAllUrlParams = UrlParams;
+export type GetOneUrlParams = UrlParams & ObjectWithID;
+
+export interface Dashboard extends ObjectWithID {
+  id: string;
+  widgets: DashboardWidget[];
+}
+
 // https://y42.com/dashboard/api/dashboards/{id: string}/widgets
 // https://y42.com/dashboard/api/dashboards/{id: string}/widgets/{id: string}
 
+export interface DashboardWidget extends ObjectWithID {
+  id: string;
+}
 
+export type DashboardWidgetUrlParam = 'dashboardId';
+export type DashboardWidgetUrlParams<T> = Record<DashboardWidgetUrlParam, T[keyof T]>;
+
+// export type DashboardWigetUrlParams = UrlParams &
 // https://y42.com/datasource/api/datasources
 // https://y42.com/datasource/api/datasources/{id: number}
+
+interface Datasource extends ObjectWithID {
+  id: number;
+  tables: DatasourceTable[];
+}
 
 // https://y42.com/datasource/api/datasources/{id: number}/tables
 // https://y42.com/datasource/api/datasources/{id: number}/tables/{id: number}
 
+export interface DatasourceTable extends ObjectWithID {
+  id: number;
+}
+
+export type DatasourceTableUrlParam = 'datasourceId';
+export type DatasourceTableUrlParams<T> = Record<DatasourceTableUrlParam, T[keyof T]>;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BaseService {
+export abstract class BaseService<T extends ObjectWithID, P extends UrlParams> {
+  constructor(private http: HttpClient) {}
 
+  abstract readonly finalUrl: string;
 
-  constructor(private http: HttpClient) {
+  getAllEntries(params?: P) {
+    const url = this.buildUrl(params);
 
+    console.log(`Fetching from ${url}`);
+    return this.http.get<T[]>(url);
   }
 
-  get finalUrl(): string {
-    return '';
+  getEntryById(id: T['id'], params?: P) {
+    const url = `${this.buildUrl(params)}/${id}`;
+
+    console.log(`Fetching from ${url}`);
+    return this.http.get<T>(`${url}/${id}`);
   }
 
-  getAllEntries() {
-    return this.http.get(this.finalUrl);
+  private buildUrl(params?: P) {
+    return params
+      ? Object.entries(params).reduce(
+          (finalUrl, [key, value]) => finalUrl.replace(`{${key}}`, `${value}`),
+          this.finalUrl
+        )
+      : this.finalUrl;
   }
-
-  getEntryById(id) {
-    return this.http.get(this.finalUrl + '/' + id);
-  }
-
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DashboardService extends BaseService {
+export class DashboardService extends BaseService<Dashboard, never> {
   constructor(http: HttpClient) {
     super(http);
   }
 
+  readonly finalUrl = '/dashboard/api/dashboards';
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DashboardWidgetService extends BaseService {
+export class DashboardWidgetService extends BaseService<
+  DashboardWidget,
+  DashboardWidgetUrlParams<DashboardWidget>
+> {
   constructor(http: HttpClient) {
     super(http);
   }
+
+  readonly finalUrl = '/dashboard/api/dashboards/{dashboardId}/widgets';
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DatasourceService extends BaseService {
+export class DatasourceService extends BaseService<Datasource, never> {
   constructor(http: HttpClient) {
     super(http);
   }
+
+  readonly finalUrl = '/datasource/api/datasources';
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DatasourceTableService extends BaseService {
+export class DatasourceTableService extends BaseService<
+  DatasourceTable,
+  DatasourceTableUrlParams<DatasourceTable>
+> {
   constructor(http: HttpClient) {
     super(http);
   }
-}
 
+  readonly finalUrl = '/datasource/api/datasources/{datasourceId}/tables';
+}
 
 @Component({
   selector: 'app-url-text',
-  template: ``
+  template: ``,
 })
 export class UrlTestComponent implements OnInit {
-
-  constructor(private dashboardService: DashboardService,
-              private dashboardWidgetService: DashboardWidgetService,
-              private datasourceService: DatasourceService,
-              private datasourceTableService: DatasourceTableService) {
-  }
+  constructor(
+    private dashboardService: DashboardService,
+    private dashboardWidgetService: DashboardWidgetService,
+    private datasourceService: DatasourceService,
+    private datasourceTableService: DatasourceTableService
+  ) {}
 
   ngOnInit(): void {
+    this.getAllDashoards().subscribe();
+    this.getDashboardById('1').subscribe();
 
+    this.getAllDashobardWidgets('1').subscribe();
+    this.getDashboardWidgetById('1', '1').subscribe();
+
+    this.getAllDatasources().subscribe();
+    this.getDatasourcesById(1).subscribe();
+
+    this.getAllDatasourceTables(1).subscribe();
+    this.getDatasourceTableById(1, 1).subscribe();
   }
 
-  getAllDashoards() {
-    this.dashboardService.getAllEntries().subscribe();
+  getAllDashoards(): Observable<Dashboard[]> {
+    return this.dashboardService.getAllEntries();
   }
 
-  getDashboardById(id: any) { // please fix any
-    this.dashboardService.getEntryById(id).subscribe();
+  getDashboardById(id: Dashboard['id']): Observable<Dashboard> {
+    return this.dashboardService.getEntryById(id);
   }
 
-  getAllDashoardWidgets() {
-
+  getAllDashobardWidgets(dashboardId: Dashboard['id']): Observable<DashboardWidget[]> {
+    return this.dashboardWidgetService.getAllEntries({ dashboardId });
   }
 
-  getDashoardWidgetById(id: any) { // please fix any
-
+  getDashboardWidgetById(id: DashboardWidget['id'], dashboardId: Dashboard['id']) {
+    return this.dashboardWidgetService.getEntryById(id, { dashboardId });
   }
 
-  getAllDatasources() {
-
+  getAllDatasources(): Observable<Datasource[]> {
+    return this.datasourceService.getAllEntries();
   }
 
-  getDatasourcesById(id: any) { // please fix any
-
+  getDatasourcesById(id: Datasource['id']): Observable<Datasource> {
+    return this.datasourceService.getEntryById(id);
   }
 
-  getAllDatasourceTables() {
-
+  getAllDatasourceTables(datasourceId: Datasource['id']): Observable<DatasourceTable[]> {
+    return this.datasourceTableService.getAllEntries({ datasourceId });
   }
 
-  getDatasourceTableById(id: any) { // please fix any
-
+  getDatasourceTableById(
+    id: DatasourceTable['id'],
+    datasourceId: Datasource['id']
+  ): Observable<DatasourceTable> {
+    return this.datasourceTableService.getEntryById(id, { datasourceId });
   }
-
 }
